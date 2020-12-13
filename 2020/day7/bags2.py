e9b40c1ae09ee4bffbbf6acca1a2778aed5f1561
Daniel Killenberger <daniel.killenberger@gmail.com>
2020-12-13 10:30:37 +0100

    Solve day ex2

--- 2020/day7/bags2.py
+++ 2020/day7/bags2.py
@@ -1,5 +1,4 @@
 import re
-from functools import reduce
 
 with open("input.txt", "r") as input_file:
     input = input_file.read().split("\n")
@@ -13,25 +12,11 @@ def parse_rule(rule_string):
     if "no other bags" in children_string:
         children = None
     else:
-        children_strings = list(map(lambda child: re.sub(" bag(s)?", "", child).replace(".", "").strip(), children_string.split(",")))
-        children = list(map(lambda child_string: {"amount": int(child_string[0]), "name": child_string[2:]}, children_strings))
+        children_strings = list(
+            map(lambda child: re.sub(" bag(s)?", "", child).replace(".", "").strip(), children_string.split(",")))
+        children = list(
+            map(lambda child_string: {"amount": int(child_string[0]), "name": child_string[2:]}, children_strings))
     return {"name": bag_name, "children": children}
-
-
-def get_direct_children_bags(rules, bag_name):
-    return [rules[child["name"]] for child in rules[bag_name]["children"]]
-
-
-def get_all_children_bags(rules, bag):
-    children = {}
-    open_list = bag["children"]
-
-    while open_list:
-        current_bag = open_list.pop()
-        open_list += [child for child in get_direct_children_bags(rules, bag["name"]) if child["name"] not in children]
-        if current_bag["name"] not in children:
-            children[current_bag["name"]] = current_bag
-    return children
 
 
 for rule_string in input:
@@ -39,10 +24,13 @@ for rule_string in input:
     rules[rule["name"]] = rule
 
 
-all_children = get_all_children_bags(rules, rules["shiny gold"])
+def amount_bags_in_bag(rules, bag_name):
+    children = rules[bag_name]["children"]
+    amount = 0
+    if children:
+        for child in children:
+            amount += child["amount"] + child["amount"] * amount_bags_in_bag(rules, child["name"])
+    return amount
 
-print(len(all_children))
 
-
-
-
+print(amount_bags_in_bag(rules, "shiny gold"))
